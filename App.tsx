277dcb1207b715c5faf496c1ef1bf6e2af5fe6cb
Ton-Jam <info.tonjam@gmail.com>
2025-07-21 03:28:42 +0100

    Resolved merge conflicts and merged remote changes

--- App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProfileScreen from "./ProfileScreen";
-
-const App = () => {
-  return <ProfileScreen closePage={() => console.log("Close clicked")} />;
-};
-
-export default App;
